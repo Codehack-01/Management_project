20bc0ebc899bd318706f77996731bd7ded53127c
Codehack <ademolaprojectdreams@gmail.com>
2023-09-20 16:38:58 +0100

    source code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
     },
     extend: {
       colors: {
+        Blue: '#1d4ed8',
+        lightBlue: '#3b82f6',
+        transparent: 'transparent',
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
